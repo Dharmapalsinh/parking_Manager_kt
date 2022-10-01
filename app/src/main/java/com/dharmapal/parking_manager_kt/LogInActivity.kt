fd537069e0cb9422bc0e3e3dc099c9544b4cd6ae
Dharmapalsinh <DharmapalsinhVaghela99099@gmail.com>
2022-10-01 13:54:31 +0530

    Initial commit

---
+++ app/src/main/java/com/dharmapal/parking_manager_kt/LogInActivity.kt
@@ -0,0 +1,13 @@
+package com.dharmapal.parking_manager_kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+
+    }
+}
